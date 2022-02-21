c4939c660e91fcca258c185feea9abe460eee279
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-02-21 11:50:19 -0500

    host config var

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
+    HOST: process.env.HOST || "0.0.0.0",
     PROXY_URL: "https://proxy.mkchat.app",
     BOT_TOKEN: "",
     BOT_PREFIX: "m?",
