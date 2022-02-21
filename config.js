2120df66ad06373e40736494975ccbf38c6dc89f
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-02-21 10:23:09 -0500

    add config for later use

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    PROXY_URL: "https://proxy.mkchat.app",
+    BOT_TOKEN: "",
+    BOT_PREFIX: "m?",
+    WEBHOOK_URL: "",
+    MODERATION_PASSWORD: ""
+};
