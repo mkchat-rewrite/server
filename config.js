1460440a4b872d8ba6476014b740daf016bad56b
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-02-21 21:10:01 -0500

    more config

--- config.js
+++ config.js
@@ -2,8 +2,24 @@ module.exports = {
     PORT: process.env.PORT || 3000,
     HOST: process.env.HOST || "0.0.0.0",
     PROXY_URL: "https://proxy.mkchat.app",
-    BOT_TOKEN: "",
+    BOT_TOKEN: "OTQ1NDU5MDkwNzIzODMxODI4.YhQdZQ.8uOLLl55QU5vZtwOUENpJh5mmP0",
     BOT_PREFIX: "m?",
     WEBHOOK_URL: "",
-    MODERATION_PASSWORD: ""
+    MODERATION_PASSWORD: "",
+    ROOMS: {
+        "945461992439808030": "main",
+        "945462013323264030": "main2",
+        "945462143610920970": "chill",
+        "945462118839369769": "mks-room",
+        "945462043325108265": "gaming",
+        "945462162523045938": "rp"
+    },
+    CHANNELS: {
+        "main": "945461992439808030",
+        "main2": "945462013323264030",
+        "chill": "945462143610920970",
+        "mks-room": "945462118839369769",
+        "gaming": "945462043325108265",
+        "rp": "945462162523045938"
+    }
 };
