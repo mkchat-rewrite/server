6a86369c95ae5c96c72718eccf59d6ce5546c7de
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-07-02 15:18:56 -0400

    more mod dashboaWERASFZC

--- web/moderation/index.js
+++ web/moderation/index.js
@@ -2,6 +2,81 @@
 * TODO: move most of the auth handling stuff to server
 */
 
+const SERVER_URL = "mkchat.app";
+const ws = new WebSocket(`wss://${SERVER_URL}/moderation`);
+
+ws.onmessage = msg => {
+    const message = JSON.parse(msg.data);
+
+    switch (message.type) {
+        case "connect":
+            const pass = localStorage.getItem("password");
+            await loadUsers(pass);
+            break;
+        default:
+            break;
+    };
+
+    console.log(message);
+};
+
+async function loadUsers(password) {
+    const res = await fetch(`https://${SERVER_URL}/users?password=${password}`);
+    const users = await res.json();
+
+    const data = document.getElementById("data");
+    data.innerText = "";
+
+    for (const user of users) {
+        const entry = document.createElement("div");
+        data.appendChild(entry);
+        entry.classList.add("entry");
+
+        const entryData = document.createElement("div");
+        entry.appendChild(entryData);
+        entryData.classList.add("entry-data");
+
+        /* -- ip -- */
+
+        const ipField = document.createElement("div");
+        entryData.appendChild(ipField);
+
+        const ipLabel = document.createElement("span");
+        ipField.appendChild(ipLabel);
+        ipLabel.innerText = "IP: ";
+
+        const ipValue = document.createElement("span");
+        ipField.appendChild(ipValue);
+        ipValue.innerText = user.ip;
+
+        /* -- name -- */
+
+        const nameField = document.createElement("div");
+        entryData.appendChild(nameField);
+
+        const nameLabel = document.createElement("span");
+        nameField.appendChild(nameLabel);
+        nameLabel.innerText = "Username: ";
+
+        const nameValue = document.createElement("span");
+        nameField.appendChild(nameValue);
+        nameValue.innerText = user.username;
+
+        /* -- id -- */
+
+        const idField = document.createElement("div");
+        entryData.appendChild(idField);
+
+        const idLabel = document.createElement("span");
+        idField.appendChild(idLabel);
+        idLabel.innerText = "ID: ";
+
+        const idValue = document.createElement("span");
+        idField.appendChild(idValue);
+        idValue.innerText = user.id;
+    };
+};
+
 // const params = Object.fromEntries((new URLSearchParams(window.location.search)).entries());
 // if (params?.code) {
 //     const accessToken = await authenticate(params.code);
