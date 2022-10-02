72e37012133fb5c2151e8de8c1633f72b3142f1a
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-10-02 14:19:32 -0400

    few more changes

--- server.ts
+++ server.ts
@@ -1,91 +1,105 @@
 import { config as dotenv } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
-import { nanoid } from "https://deno.land/x/nanoid@v3.0.0/mod.ts";
+import { createRouter, register } from "./http/index.ts";
+import { createSocketHandler, broadcast, publish, subscribe, unsubscribe, Connection } from "./websocket/index.ts";
+import { httpRequestHandler } from "./methods/httpRequestHandler.ts";
+import { tryParseJson } from "./methods/tryParseJson.ts";
 
 dotenv({ export: true });
 
 interface User {
-    ws?: WebSocket,
+    id: string,
+    socket: WebSocket,
+    ipAddr: string,
+    userAgent: string,
     username?: string,
-    room?: string,
-    id?: string,
-    ipAddr?: string,
-    userAgent?: string
+    room?: string
 };
 
 const users = new Map<string, User>();
-const messages = new Map(); // store message history in memory for now, later use redis/deta for perm storage
 
-function reqHandler(req: Request) {
-    if (req.headers.get("upgrade") !== "websocket") return new Response("Upgrade Required", { status: 426 });
-    // remove above line when http endpoints are introduced
+const router = createRouter({});
 
-    const { socket: ws, response } = Deno.upgradeWebSocket(req);
-    const id = nanoid(16);
-
-    ws.onopen = () => {
-        console.log(req.headers);
-
-        const addrs = req.headers.get("x-forwarded-for")?.split(",");
-        const ipAddr = Array.isArray(addrs) ? addrs[0] : null;
-        const userAgent = req.headers.get("user-agent");
-
-        if (!(ipAddr && userAgent)) return ws.close(1007, "Received invalid headers.");
-
-        users.set(id, { ipAddr, userAgent, ws });
-    };
-
-    ws.onmessage = (event: MessageEvent) => {
-        const {
-            type = null,
-            data = null
-        } = tryParseJson(event.data);
-
-        switch(type) {
-            case "join": {
-                const {
-                    username = null,
-                    room = null
-                } = data;
-
-                if (!(username && room)) ws.close(1007, "Received invalid data.");
-
-                users.set(id, { ...users.get(id), username, room, id });
-
-                console.log(users);
-
-                break;
-            }
-            case "message": {
-                const { text = null } = data;
-                console.log(text);
-                break;
-            }
-            case "broadcast": {
-                for (const user of users.values()) {
-                    user?.ws?.send(data);
-                };
-            } // test broadcasting data to all clients
-        }
-
-        console.log(type, data);
-    };
-
-    ws.onclose = (event: CloseEvent) => {
-        console.log(event);
-    };
-
-    return response;
-}
-
-serve(reqHandler, { port: Number(Deno.env.get("PORT")), hostname: Deno.env.get("HOST") });
-
-function tryParseJson(str: string) {
-    try {
-        return JSON.parse(str);
-    } catch {
-        return {};
+// send message to room users
+register({
+    router,
+    method: "post",
+    route: "/rooms/:room/messages",
+    handler: (req: Request) => {
+        console.log(req);
+        return new Response("test");
     }
-}
+});
 
-// spec for close events: https://github.com/Luka967/websocket-close-codes
+// respond with messages from room, preferrably have a "limit" param to specfy how many messages to return
+register({
+    router,
+    method: "get",
+    route: "/rooms/:room/messages",
+    handler: (req: Request) => {
+        console.log(req);
+        return new Response("test");
+    }
+});
+
+const wss = createSocketHandler({
+    uniqueIdLength: 16,
+    events: {
+        open: ({ id, socket, originRequest: req }: Connection) => {
+            const addrs = req.headers.get("x-forwarded-for")?.split(",");
+            const ipAddr = Array.isArray(addrs) ? addrs[0] : "local"; // change "local" to null in prod
+            const userAgent = req.headers.get("user-agent");
+
+            if (!(ipAddr && userAgent)) return socket.close(1007, "Received invalid headers.");
+
+            users.set(id, {
+                id,
+                socket,
+                ipAddr,
+                userAgent
+            });
+        },
+        message: (conn: Connection, event: MessageEvent) => {
+            const {
+                type = null,
+                data = null
+            } = tryParseJson(event.data);
+
+            const { id, socket } = conn;
+
+            const user = users.get(id);
+            if (!user) return socket.close(1008, "Client connection is invalid.");
+
+            switch(type) {
+                case "join": {
+                    const {
+                        username = null,
+                        room = null
+                    } = data;
+    
+                    if (!(username && room)) return socket.close(1007, "Received invalid data.");
+
+                    if (user?.room) unsubscribe(wss, conn);
+
+                    subscribe(wss, conn, room);
+                    users.set(id, { ...user, username, room });
+
+                    break;
+                }
+                case "message": {
+                    const { text = null } = data;
+                    console.log(text);
+                }
+                default:
+                    break;
+            }
+        },
+        close: (conn: Connection, event: CloseEvent) => {
+            // console.log("close", conn, event);
+        }
+    }
+});
+
+serve((req: Request) => {
+    return httpRequestHandler(req, router, wss);
+}, { port: 3000 });
