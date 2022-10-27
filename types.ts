deee238968f6f0f9eb76dabacb6796ba578d36cf
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-10-27 11:09:54 -0400

    .

--- types.ts
+++ types.ts
@@ -15,3 +15,11 @@ export interface ChatMessage {
     content: string,
     attachments: string[]
 };
+
+export interface UserAccount {
+    id: string, // unique id (immutable)
+    email: string,
+    username: string,
+    password: string, // DO NOT STORE PASSWORD PLAINTEXT, THIS WILL BE SALTED HASH BECAUSE SECURITY!
+    connectionAddresses: string[] // array of all ip addresses the user connects from
+};
